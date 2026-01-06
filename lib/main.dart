3a0d17accac9dd3fbf63fd1cc5fe61fd3b6e72c1
GirishTidke <girishtidke9@gmail.com>
2026-01-06 21:19:50 +0530

    removied comment

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:adon/screens/splash/splash_screen.dart';
-
-// import 'routes.dart';
-// import 'theme.dart';
-
-// void main() {
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       title: 'AdOn',
-//       theme: AppTheme.lightTheme(context),
-//       initialRoute: SplashScreen.routeName,
-//       routes: routes,
-//     );
-//   }
-// }
 
 import 'package:flutter/material.dart';
 import 'package:adon/screens/splash/splash_screen.dart';
